54abbf34123583577fdce26dfdc35bd44f0d4842
dhuynh <d.huynh68@gmail.com>
2017-03-14 21:23:05 -0400

    time delay first run

---
+++ T_Time DelayGolden/calculateTimeDelay.py
@@ -0,0 +1,79 @@
+
+import json
+import math as m
+
+#work on time delay input equation
+
+'''y = 1/(c*mu*(1-lambda/(c*mu))) + 1/mu;
+
+c is the number of scanners, lambda is the number of containers arriving per x amount of time
+
+mu is scanning capacity per x amount of time
+
+(mu is 10 per hour, can be multiplied to get days or years)'''
+
+raw_data = {}
+time_delay_output = {}
+
+def calculateFullSet(time_delay_inputs):
+	for combination in time_delay_inputs:
+		for port in time_delay_inputs[combination]:
+			h = float(time_delay_inputs[combination][port]['Total_TEU'])
+			c = float(time_delay_inputs[combination][port]['scanners'])
+			print port
+			if c and h !=0.0:
+				time_delay_output[port] = timeDelayEquation(h, c)
+	return
+
+
+def timeDelayEquation(h, c):
+	mu = float(10*24*365)
+
+	p = h/(c*mu)
+
+
+	print h, p
+
+	pnot = 0
+	pq = 0
+	time_delay = 0
+
+	pnot = calculatePnot(c,p)
+	pq = calculatePq(c,p,pnot)
+	time_delay = calculateTw(pq,p,h,mu)
+
+	return time_delay
+
+def calculateTw(pq,p,h,mu):
+	return (pq*(p/(h*(1-p))) + (1/mu))
+
+def calculatePq(c,p,pnot):
+	return (m.pow(c*p,c)/m.factorial(int(c))) * (1/(1-p)) * pnot
+
+
+def calculatePnot(c,p):
+	pnot = 0
+	for k in range(0,int(c-1)):
+		pnot += firstTerm(c,p,k) + secondTerm(c,p)
+	return pnot
+
+def firstTerm(c,p,k):
+	return (m.pow(c*p,k)/m.factorial(k))
+
+def secondTerm(c,p):
+	return (m.pow(c*p, c)/m.factorial(int(c))*(1/(1-p)))
+
+
+#Main
+
+with open("time_delay_inputs.json") as f:
+	raw_data = f.read()
+
+
+time_delay_inputs = json.loads(raw_data)
+
+calculateFullSet(time_delay_inputs)
+
+with open("time_delay_output.json", 'w') as s:
+	json.dump(time_delay_output, s, sort_keys=True)
+
